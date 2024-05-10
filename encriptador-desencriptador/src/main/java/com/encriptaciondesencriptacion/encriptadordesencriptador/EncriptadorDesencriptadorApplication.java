5ec78b785c73137ea68a9620966b923799aaad7b
zarhell <johan.zubieta@gmail.com>
2024-05-09 23:40:13 -0500

    Add files via upload

---
+++ encriptador-desencriptador/src/main/java/com/encriptaciondesencriptacion/encriptadordesencriptador/EncriptadorDesencriptadorApplication.java
@@ -0,0 +1,13 @@
+package com.encriptaciondesencriptacion.encriptadordesencriptador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncriptadorDesencriptadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncriptadorDesencriptadorApplication.class, args);
+	}
+
+}
---
+++ encriptador-desencriptador/src/test/java/com/encriptaciondesencriptacion/encriptadordesencriptador/EncriptadorDesencriptadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encriptaciondesencriptacion.encriptadordesencriptador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncriptadorDesencriptadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
